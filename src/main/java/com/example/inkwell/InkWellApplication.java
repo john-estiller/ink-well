cc79c0e31bd8c48f7a1e5f7584fb878947e1c9e0
John Estiller <johnestiller@Johns-MacBook-Pro.local>
2024-10-27 18:45:39 +0800

    Implement unit test in loading main app.

--- src/main/java/com/example/inkwell/InkWellApplication.java
+++ src/main/java/com/example/inkwell/InkWellApplication.java
@@ -2,23 +2,9 @@ package com.example.inkwell;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class InkWellApplication {
-
-  @Value("${NAME:World}")
-  String name;
-
-  @RestController
-  class HelloworldController {
-    @GetMapping("/")
-    String hello() {
-      return "Hello " + name + "!";
-    }
-  }
 
   public static void main(String[] args) {
     SpringApplication.run(InkWellApplication.class, args);
