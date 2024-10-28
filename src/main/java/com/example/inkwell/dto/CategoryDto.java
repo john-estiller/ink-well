3bcadc4aa1905ef4655275924d33e71554719dd2
John Estiller <johnestiller@Johns-MacBook-Pro.local>
2024-10-28 20:28:23 +0800

    Add and implement lombok.

--- src/main/java/com/example/inkwell/dto/CategoryDto.java
+++ src/main/java/com/example/inkwell/dto/CategoryDto.java
@@ -1,35 +1,13 @@
 package com.example.inkwell.dto;
 
-public class CategoryDto {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryDto {
     private int categoryId;
     private String name;
-
-    public CategoryDto() {
-    }
-
-    public CategoryDto(String name) {
-        this.name = name;
-    }
-
-    public CategoryDto(int categoryId, String name) {
-        this.categoryId = categoryId;
-        this.name = name;
-    }
-
-    public int getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(int categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
